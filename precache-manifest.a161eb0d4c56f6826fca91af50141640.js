b4ddaceda653285af7ef74eb19ec177bd62fb38a
billy nicart <billy.nicart@gmail.com>
2019-05-12 01:14:11 +0800

    Updates

---
+++ precache-manifest.a161eb0d4c56f6826fca91af50141640.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d1c17e661eb669b283b4a820f1f618",
+    "url": "/github-user-info/index.html"
+  },
+  {
+    "revision": "026bca60c36b5810f6be",
+    "url": "/github-user-info/static/css/main.a6551e80.chunk.css"
+  },
+  {
+    "revision": "102c4e2534990d4f9b81",
+    "url": "/github-user-info/static/js/2.429ed41a.chunk.js"
+  },
+  {
+    "revision": "026bca60c36b5810f6be",
+    "url": "/github-user-info/static/js/main.516a3895.chunk.js"
+  },
+  {
+    "revision": "6ad7ff551f269bec8981",
+    "url": "/github-user-info/static/js/runtime~main.fe7b53e7.js"
+  }
+]);
